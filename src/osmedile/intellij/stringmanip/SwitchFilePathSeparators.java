a867ee91f21f55d54303037031cff7a1f5c3d8ce
Daniel Franke <franke@edf-online.de>
2019-06-28 16:10:05 +0200

    Switch File Path Separator

--- src/osmedile/intellij/stringmanip/SwitchFilePathSeparators.java
+++ src/osmedile/intellij/stringmanip/SwitchFilePathSeparators.java
@@ -2,22 +2,46 @@ package osmedile.intellij.stringmanip;
 
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.util.Pair;
+import org.jetbrains.annotations.NotNull;
 
 public class SwitchFilePathSeparators extends AbstractStringManipAction {
 
-	@Override
-	public String transformSelection(Editor editor, DataContext dataContext, String s, Object additionalParam) {
-		String s1;
-		if (s.contains("/")) {
-			s1 = s.replace("/", "\\");
-		} else {
-			s1 = s.replace("\\", "/");
-		}
-		return s1;
-	}
+    private char slashFound = ' ';
 
-	@Override
-	public String transformByLine(String s) {
-		throw new UnsupportedOperationException();
-	}
+    @NotNull
+    @Override
+    public Pair beforeWriteAction(Editor editor, DataContext dataContext) {
+        slashFound = ' ';
+        return super.beforeWriteAction(editor, dataContext);
+    }
+
+    @Override
+    public String transformSelection(Editor editor, DataContext dataContext, String s, Object additionalParam) {
+        String s1;
+        if (slashFound == ' ') {
+            if (s.contains("/")) {
+                slashFound = '/';
+            } else if (s.contains("\\")) {
+                slashFound = '\\';
+            }
+        }
+        switch (slashFound) {
+            case '/':
+                s1 = s.replace("/", "\\");
+                break;
+            case '\\':
+                s1 = s.replace("\\", "/");
+                break;
+            default:
+                s1 = s;
+                break;
+        }
+        return s1;
+    }
+
+    @Override
+    public String transformByLine(String s) {
+        throw new UnsupportedOperationException();
+    }
 }
